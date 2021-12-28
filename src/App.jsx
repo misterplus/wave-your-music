a9347830f9981d7346beba1a232d8503c9508706
misterplus <40316318+misterplus@users.noreply.github.com>
2021-12-28 13:31:25 +0000

    looks good for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { ethers } from "ethers";
 import "./App.css";
 import abi from "./utils/WavePortal.json";
 import MIDISounds from "midi-sounds-react";
+import CryptoJS from "crypto-js";
 
 const App = () => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [midi, setMidi] = useState("");
+  const [msg, setMsg] = useState("");
+  const [musicStr, setMusicStr] = useState("none");
+  const [musicAddr, setMusicAddr] = useState("none");
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -60,22 +65,22 @@ const App = () => {
       if (ethereum) {
         const provider = new ethers.providers.Web3Provider(ethereum);
         const signer = provider.getSigner();
-        const contractAddress = "0x82D9e6cc9a0F44537262e4990e51FEeF5Fd503b4";
+        const contractAddress = "0xAb40411f60DE710F19F287892Ddd6DAf10DcEDdf";
         const contractABI = abi.abi;
-        const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const contract = new ethers.Contract(contractAddress, contractABI, signer);
 
-        let count = await wavePortalContract.getTotalWaves();
+        let count = await contract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
         /*
         * Execute the actual wave from your smart contract
         */
-        const waveTxn = await wavePortalContract.wave();
+        const waveTxn = await contract.wave(msg);
         console.log("Mining...", waveTxn.hash);
 
         await waveTxn.wait();
         console.log("Mined -- ", waveTxn.hash);
 
-        count = await wavePortalContract.getTotalWaves();
+        count = await contract.getTotalWaves();
         console.log("Retrieved total wave count...", count.toNumber());
       } else {
         console.log("Ethereum object doesn't exist!");
@@ -85,11 +90,61 @@ const App = () => {
     }
   }
 
-  var midiSounds;
+  const notes = new Map([
+    ["0", 65],
+    ["1", 66],
+    ["2", 67],
+    ["3", 68],
+    ["4", 69],
+    ["5", 70],
+    ["6", 71],
+    ["7", 72],
+    ["8", 73],
+    ["9", 74],
+    ["a", 75],
+    ["b", 76],
+    ["c", 77],
+    ["d", 78],
+    ["e", 79],
+    ["f", 80]
+  ]);
 
-  const playTestInstrument = async() => {
-		midiSounds.playChordNow(3, [60], 2.5);
-	}
+  const playMessage = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contractAddress = "0xAb40411f60DE710F19F287892Ddd6DAf10DcEDdf";
+        const contractABI = abi.abi;
+        const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let waves = await contract.getAllWaves();
+        let randomIndex = Math.floor(Math.random() * waves.length);
+        setMusicStr(waves[randomIndex].music);
+        setMusicAddr(waves[randomIndex].waver);
+        let musicCut = waves[randomIndex].music.substring(2);
+        var when = midi.contextTime();
+        var b = 0.5;
+        var music = [];
+        for (var i = 0; i < musicCut.length; i++) {
+          music.push(notes.get(musicCut[i]));
+        }
+        for (var i = 0; i < music.length; i++) {
+          midi.playChordAt(when + b * i, 0, [music[i]], b);
+        }
+      } else {
+        console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const updateMsg = async (e) => {
+    setMsg(e.target.value);
+  }
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -100,16 +155,12 @@ const App = () => {
 
       <div className="dataContainer">
         <div className="header">
-          👋 Wave your favorite music! ♪
+          👋 Wave your secret as music! ♪
         </div>
 
         <div className="bio">
-          Wanna share your favorite song? Connect your Ethereum wallet and wave it away!
+          Wanna share a secret? This site will hash your secret and turn it into piano notes! Connect your Ethereum wallet and wave it away!
         </div>
-
-        <button className="waveButton" onClick={wave}>
-          Wave your ♫
-        </button>
 
         {/*
         * If there is no currentAccount render this button
@@ -120,10 +171,21 @@ const App = () => {
           </button>
         )}
 
-        <button className="waveButton" onClick={playTestInstrument}>
-          Play Test Instrument
+        <input type="text" placeholder="Secret goes here <==" onInput={updateMsg} />
+
+        <button className="waveButton" onClick={wave}>
+          ♫ Wave it away ♫
         </button>
-        <MIDISounds ref={(ref) => (midiSounds = ref)} appElementName="root" instruments={[3]} />	
+
+        <button className="waveButton" onClick={playMessage}>
+          Play a random secret
+        </button>
+
+        <div className="bio">
+          Playing music : {musicStr} by address {musicAddr}
+        </div>
+
+        <MIDISounds ref={(ref) => (setMidi(ref))} appElementName="root" instruments={[0]} />
       </div>
     </div>
   );
